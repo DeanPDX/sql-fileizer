e24269dbf57452733ff26ac95954e3024e44979b
Dean Davidson <ddavidson85@gmail.com>
2019-01-04 10:21:32 -0800

    Fix example config files

--- Commands/GenerateSampleConfigCommand.cs
+++ Commands/GenerateSampleConfigCommand.cs
@@ -6,33 +6,17 @@ namespace SqlFileizer.Commands
 {
     public class GenerateSampleConfigCommand : ICommand
     {
-          /// <summary>
+        /// <summary>
+        /// The folder for our sample config files.
+        /// </summary>
+        private static string _sampleConfigFolder = "SampleConfigs";
+
+        /// <summary>
         /// Sample config file; default to scripting out all procs in a given database
         /// </summary>
         /// <returns></returns>
-        private static string _sampleConfigText = @"
-<config>
-    <OutputDirectory>procs</OutputDirectory>
-    <ConnectionString>Server=ServerName;Database=DatabaseName;Trusted_Connection=True;</ConnectionString>
+        private static string _sampleConfigFile = "ScriptsFromProcs.xml";
 
-    <!-- Result set is parsed based on column names, not order -->
-    <!-- FileName: name of file without extension -->
-    <!-- FileExtension: do not include the dot -->
-    <!-- BodyText: only handles text output for now (no binary support) -->
-    <!-- HeaderText: to be appended to the beginning of each file; separated from body by ""go"" directive -->
-    <!-- FooterText: to be appended to the end of each file; separated from body by ""go"" directive -->
-    <SqlQuery>
-        select o.name as FileName,
-	        'sql' as FileExtension,
-	        definition as BodyText,
-	        'if object_id(''' + o.name + ''') is not null drop procedure ' + o.name as HeaderText,
-	        '' as FooterText
-        from sys.sql_modules m
-	        join sys.objects o on m.object_id = o.object_id
-        where o.type_desc = 'SQL_STORED_PROCEDURE'
-    </SqlQuery>
-</config>
-";
         public string CommandName => "config";
 
         public char CommandShortcut => '3';
@@ -46,9 +30,11 @@ namespace SqlFileizer.Commands
             string fileName = "config.xml";
             string currentDirectory = Environment.CurrentDirectory;
 
+            string configText = File.ReadAllText(Path.Combine(currentDirectory, _sampleConfigFolder, _sampleConfigFile));
+
             Console.WriteLine($"Creating {fileName} in {currentDirectory}");
 
-            File.WriteAllText(Path.Combine(currentDirectory, fileName), _sampleConfigText);
+            File.WriteAllText(Path.Combine(currentDirectory, fileName), configText);
 
             Console.WriteLine();
             Console.WriteLine($"{fileName} created in {Environment.CurrentDirectory}");
